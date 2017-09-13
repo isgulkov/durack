560210db35ecf54c4b47453736adeab12433bb46
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-09-14 00:17:06 +0300

    Draw rectangle on canvas

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@
  * Created by frnkymac on 14/9/17.
  */
 
-alert("o priv");
+$(document).ready(function() {
+    var ctx = document.getElementById('main_canvas').getContext('2d');
+
+    ctx.fillStyle = 'rgb(255, 100, 100)';
+    ctx.fillRect(10, 10, 490, 490);
+});
