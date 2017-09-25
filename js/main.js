aee97e2208eac5da459e6973d1a99788ba0ba748
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-09-26 02:45:29 +0300

    Add functionality to draw the back of a card

--- js/main.js
+++ js/main.js
@@ -11,12 +11,20 @@ var cardSpritesImg = new Image(); // TODO: replace data uri with url/onload
 cardSpritesImg.src = 'img/cards.gif';
 
 var getCardOffset = function(card) {
-    var y = CARD_HEIGHT * ['hearts', 'diamonds', 'clubs', 'spades'].indexOf(card.suit);
-    var x = CARD_WIDTH * ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'].indexOf(card.value);
+    if(card === 'back') {
+        return {
+            x: 5 * CARD_WIDTH,
+            y: 4 * CARD_HEIGHT
+        }
+    }
+    else {
+        var y = CARD_HEIGHT * ['hearts', 'diamonds', 'clubs', 'spades'].indexOf(card.suit);
+        var x = CARD_WIDTH * ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'].indexOf(card.value);
 
-    return {
-        x: x,
-        y: y
+        return {
+            x: x,
+            y: y
+        }
     }
 };
 
