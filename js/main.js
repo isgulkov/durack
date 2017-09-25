f257fa2069054ebfd29c6d6caa58ddbbb9849be4
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-09-26 00:44:22 +0300

    Add gradient backdrop and stub for "draw card" method

--- js/main.js
+++ js/main.js
@@ -2,9 +2,40 @@
  * Created by frnkymac on 14/9/17.
  */
 
-$(document).ready(function() {
-    var ctx = document.getElementById('main_canvas').getContext('2d');
+'use strict';
 
-    ctx.fillStyle = 'rgb(255, 100, 100)';
-    ctx.fillRect(10, 10, 490, 490);
+var CARD_WIDTH = 69;
+var CARD_HEIGHT = 94;
+
+var drawCard = function(card, x, y) {
+    console.log("card " + card + x + y);
+
+    this.save();
+
+    this.fillStyle = 'white';
+    this.fillRect(x, y, CARD_WIDTH, CARD_HEIGHT);
+
+    this.fillStyle = 'black';
+    this.fillText("Card " + card.suit + " " + card.value, x + 5, y + 50);
+
+    this.restore();
+};
+
+$(document).ready(function() {
+    var canvas = document.getElementById('main_canvas');
+    var ctx = canvas.getContext('2d');
+
+    ctx.drawCard = drawCard;
+
+    var grad = ctx.createLinearGradient(0, 0, 0, 600);
+
+    grad.addColorStop(0.0, '#afa');
+    grad.addColorStop(0.75, '#0c0');
+    grad.addColorStop(1.0, '#070');
+
+    ctx.fillStyle = grad;
+
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.drawCard({suit: 'S', value: '10'}, 10, 10);
 });
